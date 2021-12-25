ddfc445ebccacc283c3271835baad7733f69c0f9
이현홍 <s_1012@naver.com>
2021-12-25 12:40:32 +0900

    homepage

--- Lotto/urls.py
+++ Lotto/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import path, include
+from ..recommend.views import index
 
 
 urlpatterns = [
+    path('/', index),
     path('admin/', admin.site.urls),
     path('recommend/', include('recommend.urls')),
     path('detail/<int:num>', include('recommend.urls')),
